35e20e0860c6a4c7fe4b8ad4ab6cbc1595544b1f
qanhp <146439085+qanhp@users.noreply.github.com>
2024-01-06 19:43:21 +0100

    Add clear function and 2nd mode

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let grid_container = document.querySelector('.grid_container');
 
+//Grid erstellen
 for (let i=0; i<16; i++) {
     let square = document.createElement('div');
     square.className = 'square';
@@ -12,8 +13,90 @@ for (let i=0; i<16; i++) {
 
     let width = square.offsetWidth;
     let newWidth = 4*width;
-    grid_container.style.maxWidth = newWidth + 'px';
+    grid_container.style.maxWidth = newWidth + 'px';   
+}
+//funktionalität squares schwarz ausfüllen
+    // Prevent default dragstart event to disable dragging
+document.addEventListener('dragstart', function(event) {
+    event.preventDefault();
+});
+    // fill auch möglich wenn linksklick gedrückt gehalten
+let isMousedown = false;
+document.addEventListener('mousedown', function() {
+    isMousedown = true;
+})
+document.addEventListener('mouseup', function() {
+    isMousedown = false;
+})
 
-    
+let fill = function(div) {
+    if (mode===0) {
+        if (div.style.backgroundColor == 'white') {
+            div.style.backgroundColor = 'black';
+        } 
+    } else if (mode===1) {
+        if (div.style.backgroundColor == 'white') {
+            div.style.backgroundColor = 'black';
+        } 
+         else if (div.style.backgroundColor == 'black') {
+            div.style.backgroundColor = 'white';
+        }   
+    }
 }
 
+const addClick = function(div) {
+    div.addEventListener( 'mouseenter', function(event) {
+        if (isMousedown) {
+            fill(div);
+        }
+    });
+
+    div.addEventListener( 'mousedown', function() {
+        fill(div);
+    });
+}
+
+let squareClass = document.querySelectorAll(".square");
+squareClass.forEach(addClick);
+
+//funktionalität clear bzw. grid zurücksetzen
+let clear_button = document.querySelector('#clear');
+clear_button.addEventListener('click', function() {
+    squareClass.forEach(clear);
+});
+
+const clear = function(div) {
+    div.style.backgroundColor = 'white';
+}
+
+//mode
+let mode = 0;
+let mode_button = document.querySelector('#mode');
+
+const switchMode = function() {
+        // if (mode===0) {
+        //     fill = function(div) {
+        //         if (div.style.backgroundColor == 'white') {
+        //             div.style.backgroundColor = 'black';
+        //         } 
+        //          else if (div.style.backgroundColor == 'black') {
+        //             div.style.backgroundColor = 'white';
+        //         }   
+        //     }
+        //     mode = 1;
+        // }
+        // if (mode===1) {
+        //     fill = function(div) {
+        //         if (div.style.backgroundColor == 'white') {
+        //             div.style.backgroundColor = 'black';
+        //         } 
+        //         //  else if (div.style.backgroundColor == 'black') {
+        //         //     div.style.backgroundColor = 'white';
+        //         // }   
+        //     }
+        //     mode = 0;
+        // }
+        mode = mode === 0 ? 1 : 0;
+}
+
+mode_button.addEventListener('click', switchMode) 
